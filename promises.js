7e31f5de60b2708f90aa9bb023a59bacd5459cf8
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-02 18:06:23 +0530

    promises in async await

--- promises.js
+++ promises.js
@@ -52,3 +52,25 @@ promiseFour
 //
 //
 //
+
+const promiseFive = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "12345" });
+    } else {
+      reject("Error : JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
